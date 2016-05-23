edcafdbf4564da997740925e1536dc10db9f059f
escoand <escoand@users.noreply.github.com>
2016-05-23 15:25:46 +0200

    custom about dialog

--- app/src/main/java/com/escoand/android/daily_read/AboutActivity.java
+++ app/src/main/java/com/escoand/android/daily_read/AboutActivity.java
@@ -17,13 +17,16 @@
 
 package com.escoand.android.daily_read;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +44,29 @@ public class AboutActivity extends AppCompatActivity {
         });
 
         ViewGroup v = (ViewGroup) findViewById(R.id.content);
-        v.addView(getLayoutInflater().inflate(R.layout.activity_about, null));
+        v.addView(getLayoutInflater().inflate(R.layout.fragment_about, null));
+
+        ((TextView) v.findViewById(R.id.about_version)).setText(getString(R.string.about_version) + " " + BuildConfig.VERSION_NAME);
+        v.findViewById(R.id.about_recommend).setOnClickListener(this);
+        v.findViewById(R.id.about_tickets).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent();
+        switch (v.getId()) {
+            case R.id.about_recommend:
+                i.setAction(Intent.ACTION_SEND);
+                i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.message_recommend_subject));
+                i.putExtra(Intent.EXTRA_TEXT, getString(R.string.message_recommend) + BuildConfig.APPLICATION_ID);
+                i.setType("text/plain");
+                break;
+            case R.id.about_tickets:
+                i.setAction(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(getString(R.string.url_tickets)));
+                break;
+        }
+        if (i.getAction() != null)
+            startActivity(i);
     }
 }
