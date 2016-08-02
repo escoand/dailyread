3dc06d38236b11d85320f1b5a4698704a6c63be2
escoand <escoand@users.noreply.github.com>
2016-08-02 22:21:09 +0200

    refactoring

--- app/src/main/java/com/escoand/android/readdaily/CalendarDialogFragment.java
+++ app/src/main/java/com/escoand/android/readdaily/CalendarDialogFragment.java
@@ -25,6 +25,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.view.ContextThemeWrapper;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
