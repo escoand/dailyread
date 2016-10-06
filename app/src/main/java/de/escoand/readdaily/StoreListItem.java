d5e89ff478aaec6ea9e5613f48a754038d772770
escoand <escoand@users.noreply.github.com>
2016-10-06 21:46:25 +0200

    more speaking download title

--- app/src/main/java/de/escoand/readdaily/StoreListItem.java
+++ app/src/main/java/de/escoand/readdaily/StoreListItem.java
@@ -157,8 +157,7 @@ public class StoreListItem {
                             activity,
                             transaction.purchaseInfo.signature,
                             transaction.purchaseInfo.responseData,
-                            listing.title,
-                            0
+                            (String) title.getText()
                     );
                     refreshButton();
                 }
