b34a018cd5cb2c3f6e4b7d5fcb25a31a9e4c4163
escoand <escoand@users.noreply.github.com>
2016-12-20 21:17:09 +0100

    fix crash on page swipe back

--- app/src/main/java/de/escoand/readdaily/EndlessContentPager.java
+++ app/src/main/java/de/escoand/readdaily/EndlessContentPager.java
@@ -22,7 +22,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
@@ -108,7 +108,7 @@ public class EndlessContentPager extends ViewPager implements OnDateSelectedList
                 tmp.onDateSelected(date);
     }
 
-    private class EndlessDayPagerAdapter extends FragmentPagerAdapter {
+    private class EndlessDayPagerAdapter extends FragmentStatePagerAdapter {
         public EndlessDayPagerAdapter(FragmentManager fm) {
             super(fm);
         }
