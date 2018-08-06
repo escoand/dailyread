1d8469717e1c473f2118fa18e417ebaa919ba67b
escoand <escoand@users.noreply.github.com>
2018-08-06 18:25:51 +0200

    move test data to file

--- app/src/androidTest/java/de/escoand/readdaily/DatabaseTest.java
+++ app/src/androidTest/java/de/escoand/readdaily/DatabaseTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 escoand.
+ * Copyright (c) 2018 escoand.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,7 +30,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 
-import java.io.ByteArrayInputStream;
 import java.util.GregorianCalendar;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -86,12 +85,7 @@ public class DatabaseTest {
     // test csv file handling
     @Test
     public void test_11_CSV() throws Exception {
-        String data = "01.01\t20001101\tAuthor 1\tScripture 1\tScr 1\tTitle 1\tExegesis 1\tVerse Week 1\tScripture Week 1\tVerse Month 1\tScripture Month 1\n" +
-                "02.02\t20001102\tAuthor 2\tScripture 2\tScr 2\tTitle 2\tExegesis 2\tVerse Week 1\tScripture Week 1\t\t\n" +
-                "03.03\t20001103\tAuthor 3\tScripture 3\tScr 3\tTitle 3\tExegesis 3\t\t\tVerse Month 3\tScripture Month 3\n" +
-                "04.04\t20001104\tAuthor 4\tScripture 4\tScr 4\tTitle 4\tExegesis 4\t\t\t\t";
-
-        db.importCSV(TEST_CSV, new ByteArrayInputStream(data.getBytes()));
+        db.importCSV(TEST_CSV, getClass().getClassLoader().getResourceAsStream("raw/TestDataImport.csv"));
         Assert.assertTrue(db.isInstalled(TEST_CSV));
 
         Assert.assertEquals(4, db.getDay(new GregorianCalendar(2000, 11 - 1, 1).getTime()).getCount());
@@ -109,27 +103,7 @@ public class DatabaseTest {
     // test xml file handling
     @Test
     public void test_12_XML() throws Exception {
-        String data = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
-                "<collection creationDate=\"01.01.1970 01:02:03\">" +
-                "<entry sourceId=\"123\" source=\"Name of the Book 1\" title=\"Title of the Book 1\" subtitle=\"Subtitle of the Book 1\">Description of the Book 1</entry>" +
-                "<entry sourceId=\"567\" source=\"Name of the Book 2\" title=\"Title of the Book 2\" subtitle=\"Subtitle of the Book 2\">Description of the Book 2</entry>" +
-                "<entry date=\"2000-01-01\" description=\"Name of the Day 1\">" +
-                "<exegesis sourceId=\"123\" source=\"Name of the Book\" sourceChapter=\"234\" sourceVerse=\"345\" title=\"Title of the Day\" subtitle=\"Subtitle of the Day\">Text of the Day</exegesis>" +
-                "<verse_of_the_day source=\"Scripture of the Day\">Verse of the Day</verse_of_the_day>" +
-                "<verse_of_the_week source=\"Scripture of the Week\">Verse of the Week</verse_of_the_week>" +
-                "<verse_of_the_month source=\"Scripture of the Month\">Verse of the Month</verse_of_the_month>" +
-                "<verse_of_the_year source=\"Scripture of the Year\">Verse of the Year</verse_of_the_year>" +
-                "</entry>" +
-                "<entry date=\"2000-01-02\" description=\"Name of the Day 2\">" +
-                "<exegesis sourceId=\"123\" source=\"Name of the Book\" sourceChapter=\"234\" sourceVerse=\"345\" title=\"Title of the Day\" subtitle=\"Subtitle of the Day\">Text of the Day</exegesis>" +
-                "<verse_of_the_day source=\"Scripture of the Day\">Verse of the Day</verse_of_the_day>" +
-                "<verse_of_the_week source=\"Scripture of the Week\">Verse of the Week</verse_of_the_week>" +
-                "<verse_of_the_month source=\"Scripture of the Month\">Verse of the Month</verse_of_the_month>" +
-                "<verse_of_the_year source=\"Scripture of the Year\">Verse of the Year</verse_of_the_year>" +
-                "</entry>" +
-                "</collection>";
-
-        db.importXML(TEST_XML, new ByteArrayInputStream(data.getBytes()));
+        db.importXML(TEST_XML, getClass().getClassLoader().getResourceAsStream("xml/TestDataImport.xml"));
         Assert.assertTrue(db.isInstalled(TEST_XML));
 
         Assert.assertEquals(5, db.getDay(new GregorianCalendar(2000, 1 - 1, 1).getTime()).getCount());
